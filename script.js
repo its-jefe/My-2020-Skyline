a2272807698ca257920dd399d7667e8e61a930ca
Jeff Jobs <Jeff.Timson@Yahoo.com>
2022-05-22 13:32:31 -0400

    messing with size and aspect

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ const loader = new THREE.ObjectLoader();
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(100, 2, 1, 200);
 
 const renderer = new THREE.WebGLRenderer({ alpha: true } );
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, (window.innerWidth/1.618));
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: '#000000' } );
 const cube = new THREE.Mesh( geometry, material );
 // scene.add( cube );
 
-camera.position.z = 100;
+camera.position.z = 80;
+camera.position.y = 2.5;
+camera.position.x = 0;
+// camera.zoom(1.2);
+
+// console.log(camera)
 
 loader.load(
 	// resource URL
@@ -31,7 +36,7 @@ loader.load(
 
 	// onProgress callback
 	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+		// console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
 	},
 
 	// onError callback
@@ -40,13 +45,13 @@ loader.load(
 	}
 );
 
-console.log(scene)
+// console.log(scene)
 
 function animate() {
   requestAnimationFrame( animate );
 
   // scene.rotation.x += 0.01;
-  scene.rotation.y += 0.0035;
+  scene.rotation.y += 0.005;
 
   renderer.render( scene, camera );
 };
