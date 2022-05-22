388f89dc8350e1900509823b4a2b456f9807d06b
Jeff Jobs <Jeff.Timson@Yahoo.com>
2022-05-22 15:50:18 -0400

    still messing with ratios and view

--- script.js
+++ script.js
@@ -2,56 +2,69 @@ THREE.Cache.enabled = true;
 
 const loader = new THREE.ObjectLoader();
 
-
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(100, 2, 1, 200);
 
 const renderer = new THREE.WebGLRenderer({ alpha: true } );
-renderer.setSize(window.innerWidth, (window.innerWidth/1.618));
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry();
+
+console.log(geometry)
 const material = new THREE.MeshBasicMaterial( { color: '#000000' } );
 const cube = new THREE.Mesh( geometry, material );
 // scene.add( cube );
 
-camera.position.z = 80;
-camera.position.y = 2.5;
-camera.position.x = 0;
-// camera.zoom(1.2);
+scene.rotation.y = 0;
+console.log(camera.position);
 
-// console.log(camera)
+console.log(camera.position);
 
 loader.load(
 	// resource URL
 	"./model.json",
-
+	
 	// onLoad callback
 	// Here the loaded data is assumed to be an object
 	function ( obj ) {
 		// Add the loaded object to the scene
 		scene.add( obj );
 	},
-
+	
 	// onProgress callback
 	function ( xhr ) {
 		// console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
 	},
-
+	
 	// onError callback
 	function ( err ) {
 		console.error( 'An error happened' );
 	}
-);
+	);
+	
+	// console.log(scene)
+	
+	let x = 0;
+	let y = 0;
+	camera.position.z = (-25) * Math.cos(Math.PI * x) + 75;
+	
+	function animate() {
+		requestAnimationFrame( animate );
+		x += 0.004;
+		y += 0.001;
+		scene.rotation.y = (2 * Math.PI * y)
+		// needs to hit 100 and back again every half rotation
+		// camera.position.z = (-25) * Math.cos(Math.PI * x) + 75;
+		// console.log(scene.rotation.y)
+		
+		// console.log(camera.position)
+		
+		// 
+		
 
-// console.log(scene)
 
-function animate() {
-  requestAnimationFrame( animate );
-
-  // scene.rotation.x += 0.01;
-  scene.rotation.y += 0.005;
+	// console.log(camera.position.z)
 
   renderer.render( scene, camera );
 };
